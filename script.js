b88ea3bb1c2fe6233a9eff79fadc84b052e6e635
tymtor <mohammadgag140@gmail.com>
2024-12-08 20:12:34 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// عند الضغط على زر تحميل الأداة
+document.getElementById('downloadBtn').addEventListener('click', function() {
+    // هنا يمكنك إضافة رابط لتحميل الأداة
+    window.location.href = 'رابط تحميل الأداة.exe';
+});
